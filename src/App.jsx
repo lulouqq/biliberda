7326d6b1df4c5e5ce9936d2699124e7e9d84756a
Nikita Veliev <nik.veliev@inbox.ru>
2026-02-19 01:48:03 +0300

    Adjust zoom to 18

--- src/App.jsx
+++ src/App.jsx
@@ -47,7 +47,7 @@ function App() {
         geoPos.current = pos.coords;
         const userLatLng = L.latLng(geoPos.current.latitude, geoPos.current.longitude);
         if(!hasCenteredRef.current){
-          mapRef.current.setView(userLatLng, 20);
+          mapRef.current.setView(userLatLng, 18);
           hasCenteredRef.current = true;
         }
         console.log("Geo update: ", geoPos);
